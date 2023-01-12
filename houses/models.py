96ba3e40a2f960c4e922c78986b5d06c4b4c9ba9
lifeissea <lifeissea@naver.com>
2023-01-12 16:24:51 +0900

    add Custom User

--- houses/models.py
+++ houses/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
+
 class House(models.Model):
 
     """Model Definition for Houses"""
 
     name = models.CharField(max_length=140)
-    price_per_night = models.PositiveIntegerField(verbose_name="Price", help_text="Positive Number Only")
+    price_per_night = models.PositiveIntegerField(
+        verbose_name="Price", help_text="Positive Number Only")
     description = models.TextField()
     address = models.CharField(max_length=140)
     pets_allowed = models.BooleanField(
