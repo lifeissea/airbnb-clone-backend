a19d1c2470fdbf7d0493fa09fcdf928f13abe070
lifeissea <lifeissea@naver.com>
2023-01-13 13:04:50 +0900

    add rooms admin list_play

--- rooms/models.py
+++ rooms/models.py
@@ -13,6 +13,7 @@ class Room(CommonModel):
         PRIVATE_ROOM = ("private_room", "Private Room")
         SHARED_ROOM = ("shared_room", "Shared Room")
 
+    name = models.CharField(max_length=180, default="",)
     country = models.CharField(max_length=50, default="한국",)
     city = models.CharField(max_length=80, default="서울",)
     price = models.PositiveIntegerField()
@@ -26,6 +27,9 @@ class Room(CommonModel):
     """Many to Many Amenity"""
     amenities = models.ManyToManyField("rooms.Amenity")
 
+    def __str__(self) -> str:
+        return self.name
+
 
 class Amenity(CommonModel):
 
@@ -33,3 +37,9 @@ class Amenity(CommonModel):
 
     name = models.CharField(max_length=150)
     description = models.CharField(max_length=150, null=True, blank=True,)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Amenities"
