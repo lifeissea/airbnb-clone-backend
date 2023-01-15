b544b212ead4c8862203237e82ae1864522d2ee4
lifeissea <lifeissea@naver.com>
2023-01-15 21:19:54 +0900

    ~ 9.4 complete

--- rooms/views.py
+++ rooms/views.py
@@ -5,7 +5,14 @@ from .models import Room
 
 def see_all_rooms(request):
     rooms = Room.objects.all()
-    return render(request, "all_rooms.html", {"rooms": rooms, "title": "Hello! this title comes from django!"},)
+    return render(
+        request,
+        "all_rooms.html",
+        {
+            "rooms": rooms,
+            "title": "Hello! this title comes from django!",
+        },
+    )
 
 
 def see_one_rom(request, room_id):
