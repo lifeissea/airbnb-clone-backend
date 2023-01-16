ef8bdd8cf7b36325dbd062f2bf8ab7cac0ad7212
lifeissea <lifeissea@naver.com>
2023-01-16 14:46:14 +0900

    10.2 complete
    많은 옵션 가져오는 경우 many = True 넣어줘야함 꼭

---
+++ categories/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+
+
+class CategorySerializer(serializers.Serializer):
+
+    name = serializers.CharField(required=True)
+    kind = serializers.CharField()
+    created_at = serializers.DateTimeField()
