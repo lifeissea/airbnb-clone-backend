b2df8adae084b880c1df5fe933c59980f1d061f8
lifeissea <lifeissea@naver.com>
2023-01-16 20:31:13 +0900

    10.7 complete

--- categories/views.py
+++ categories/views.py
@@ -1,4 +1,5 @@
 from rest_framework.decorators import api_view
+from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
 from .models import Category
 from .serializers import CategorySerializer
@@ -6,7 +7,6 @@ from .serializers import CategorySerializer
 
 @api_view(["GET", "POST"])
 def categories(request):
-
     if request.method == "GET":
         all_categories = Category.objects.all()
         serializer = CategorySerializer(all_categories, many=True)
@@ -22,8 +22,24 @@ def categories(request):
             return Response(serializer.errors)
 
 
-@api_view()
+@api_view(["GET", "PUT"])
 def category(request, pk):
-    category = Category.objects.get(pk=pk)
-    serializer = CategorySerializer(category)
-    return Response(serializer.data)
+    try:
+        category = Category.objects.get(pk=pk)
+    except Category.DoesNotExist:
+        raise NotFound
+
+    if request.method == "GET":
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        serializer = CategorySerializer(
+            category,
+            data=request.data,
+            partial=True,
+        )
+        if serializer.is_valid():
+            updated_category = serializer.save()
+            return Response(CategorySerializer(updated_category).data)
+        else:
+            return Response(serializer.errors)
